401953653862c8c9d37cbe4b04612d64be422810
Rayudu Veera Venkata Swamy <swamyrayudu7288@gmail.com>
2025-01-23 21:37:20 +0530

    address backend completed

--- server/controlers/shop/address-controllers.js
+++ server/controlers/shop/address-controllers.js
@@ -1,5 +1,29 @@
+const Address = require("../../models/Address");
+
 const addAdress = async (req, res) => {
   try {
+    const { address, userId, city, pincode, phone, note } = req.body;
+
+    if (!address || !userId || !city || !pincode || !phone || !note) {
+      res.status(404).json({
+        success: false,
+        message: "Invalid data provided",
+      });
+    }
+
+    const newlycreatedaddress = Address({
+      userId,
+      address,
+      city,
+      pincode,
+      phone,
+      note,
+    });
+    await newlycreatedaddress.save();
+    res.status(201).json({
+      success: true,
+      data: newlycreatedaddress,
+    });
   } catch (e) {
     console.log(e);
     res.status(500).json({
@@ -11,6 +35,18 @@ const addAdress = async (req, res) => {
 
 const fetchAdress = async (req, res) => {
   try {
+    const { userId } = req.params;
+    if (!userId) {
+      res.status(404).json({
+        success: false,
+        message: "Invalid data provided",
+      });
+    }
+    const addressList = await Address.find({ userId });
+    res.status(201).json({
+      success: true,
+      data: addressList,
+    });
   } catch (e) {
     console.log(e);
     res.status(500).json({
@@ -22,6 +58,34 @@ const fetchAdress = async (req, res) => {
 
 const editAdress = async (req, res) => {
   try {
+    const { userId, addressId } = req.params;
+    const formdata = req.body;
+    if (!userId || !addressId) {
+      res.status(404).json({
+        success: false,
+        message: "Invalid data provided",
+      });
+    }
+
+    const address = await Address.findOneAndUpdate(
+      {
+        _id: addressId,
+        userId,
+      },
+      formdata,
+      { new: true }
+    );
+    if (!address) {
+      return res.status(404).json({
+        success: false,
+        message: "Address not found",
+      });
+    }
+
+    res.status(201).json({
+      success: true,
+      data: address
+    })
   } catch (e) {
     console.log(e);
     res.status(500).json({
@@ -33,6 +97,27 @@ const editAdress = async (req, res) => {
 
 const deleteAdress = async (req, res) => {
   try {
+    const { userId, addressId } = req.params;
+    if (!userId || !addressId) {
+      res.status(404).json({
+        success: false,
+        message: "Invalid data provided",
+      });
+    }
+    const address = await Address.findByIdAndDelete({
+      _id: addressId,userId
+    })
+    if (!address) {
+      return res.status(404).json({
+        success: false,
+        message: "Address not found",
+      });
+    }
+
+    res.status(201).json({
+      success: true,
+      message: "Delete Address"
+    })
   } catch (e) {
     console.log(e);
     res.status(500).json({
