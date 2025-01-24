e4434b046638264800c21d5e0294784382144387
Rayudu Veera Venkata Swamy <swamyrayudu7288@gmail.com>
2025-01-24 19:24:03 +0530

    order and address files are created basic part

--- client/src/pages/shopping-view/account.jsx
+++ client/src/pages/shopping-view/account.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
 
 export default function ShoppingAccount() {
   return (
     <div className="flex flex-col">
       <div className=" relative h-[350px] w-full overflow-hidden">
-        <img src="https://github.com/sangammukherjee/mern-ecommerce-2024/blob/master/client/src/assets/account.jpg?raw=true" alt="image" className="" />
+        <img
+          src="https://github.com/sangammukherjee/mern-ecommerce-2024/blob/master/client/src/assets/account.jpg?raw=true"
+          alt="image"
+          className=""
+        />
+      </div>
+      <div className=" container mx-auto grid grid-cols-1 gap-8 py-8">
+          <div className="flex flex-col rounded-lg border bg-background p-6 shadow-md">
+              <Tabs defaultValue="orders">
+                  <TabsList>
+                    <TabsTrigger value='orders'>Orders</TabsTrigger>
+                    <TabsTrigger value='address'>Address</TabsTrigger>
+                  </TabsList>
+                  <TabsContent value='orders'>
+                      
+                  </TabsContent>
+                  <TabsContent value='address'>
+
+                  </TabsContent>
+              </Tabs>
+          </div>
       </div>
     </div>
   );
